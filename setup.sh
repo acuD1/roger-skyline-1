a86e84a4ed5db0567f28066d5a69989a77bd2f42
acuD1 <arsciand@student.42.fr>
2019-01-17 11:22:13 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Used iso : mini.iso (netboot) ubuntu 18.04
+# This script is executed post install. Before that a local script is used to install openssh-server and 
+# add id_rsa.pub to the VM. Then, roger-skyline-1.git is cloned to run the deployment.
+# gitclone.sh
+# >>>>
+# #!/bin/bash
+# sudo apt update -y && sudo apt upgrade -y
+# sudo apt install git openssh-server -y
+# git clone https://github.com/acuD1/roger-skyline-1.git
+# ====
+#
 # Initialisation for deployment
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install vim ccze python3.6 python-pip -y
