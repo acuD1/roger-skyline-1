9ea7440be1ec6d3c90c487e09f605a8dcad03d11
acuD1 <arsciand@student.42.fr>
2019-01-17 17:02:53 +0100

    Update

--- rerun.sh
+++ rerun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ~/ansible-play/.venv/bin/activate
-ansible-playbook -i ./hosts ~/roger-skyline-1/Ansible/rsi-setup.yml
+ansible-playbook -i ./hosts ~/roger-skyline-1/Ansible/rs1-setup.yml
