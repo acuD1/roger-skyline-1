d121c619431ecd9f5aeee1c3e9fc55b3065e5012
acuD1 <arsciand@student.42.fr>
2019-01-17 11:04:20 +0100

    Update setup.sh with venv setup for ansible

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # Initialisation for deployment
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install vim ccze ansible -y
+sudo apt install vim ccze python3.6 python-pip
+sudo pip install -U virtualenv
+mkdir ansible-play
+cd ansible-play
+virtualenv .venv
+source .venv/bin/activate
+pip install ansible
